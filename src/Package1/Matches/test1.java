7318e1cfce01ace621be07a92d0d5c082571e8a3
Qoreas <buyfrtscbhbivpz@gmail.com>
2023-02-12 13:02:10 +0300

    matches

---
+++ src/Package1/Matches/test1.java
@@ -0,0 +1,8 @@
+package Package1.Matches;
+
+public class test1 {
+    public static void main(String[] args) {
+        String str = "Hi, world";
+        System.out.println(str.matches("(.*)world"));
+    }
+}
