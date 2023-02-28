b83dfbffa40018e1e39603f3b8995dbab8485fef
Qoreas <buyfrtscbhbivpz@gmail.com>
2023-02-28 19:55:13 +0300

    test

---
+++ src/ProlangCourse/stringTest.java
@@ -0,0 +1,10 @@
+package ProlangCourse;
+
+public class stringTest {
+    public static void main(String[] args) {
+        String a = "ABCD";
+        String b = a.toLowerCase();
+        b.replace('a', 'b');
+        System.out.println(b);
+    }
+}
